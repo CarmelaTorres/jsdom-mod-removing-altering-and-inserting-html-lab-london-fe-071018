41f814e43128d8df8cc4f9c36a10c9a55bc86bcb
Carmela Torres Molleda <carmela.torres@wework.com>
2018-08-07 17:58:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
-
+main.remove()
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+var newHeader = document.createElement('H1');
+newHeader.id = 'victory';
+newHeader.innerText = "Carmela is the champion!";
+body.appendChild(newHeader);
+  
+
+
