a82023feb46df85b96aa63e8a4b4dbe019a0e976
Carmela Torres Molleda <carmela.torres@wework.com>
2018-08-07 17:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 /* Enter the code to remove the main node element under this comment */
 main.remove()
 /* Create your new element here and assign it to newHeader */
+<<<<<<< HEAD
 
 var newHeader = document.createElement('H1');
 newHeader.id = 'victory';
 newHeader.innerText = "Carmela is the champion!";
 body.appendChild(newHeader);
+=======
+function addElement () {
+  var H1#Victory = document.createElement("h1"); 
+  var newHeader = document.createTextNode("Carmela is the Champion!");
+  newDiv.appendChild(newContent);
+}
+
+>>>>>>> fbbf3094020914ffe208351ab94605ad9daa7257
   
 
 
